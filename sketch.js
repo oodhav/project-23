15622daf415e99d2d211db150c8cd1f95baec635
oodhav <80772330+oodhav@users.noreply.github.com>
2021-10-14 07:43:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,126 +1,105 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
-
-var score = 0
-
-var gameState = "onSling";
-
-function preload() {
-    
-    getBackgroundImg();
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+	engine = Engine.create();
+	world = engine.world;
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
 
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-}
-
-function draw(){
-
-    if(backgroundImg)
-    background(backgroundImg)
+ 	boxPosition=width/2-100
+ 	boxY=610;
 
 
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
 
-    textSize(35)
-    fill("white")
-    text("score : "+score,width-300,50)
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
 
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    pig1.score();
-    log1.display();
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
 
-    box5.display();
-    log4.display();
-    log5.display();
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
 
-    bird.display();
-    platform.display();
-    pig3.score()
-    //log6.display();
-    slingshot.display();
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
 
-     
-}
 
-function mouseDragged(){
-   // if (gameState!=="launched"){
-     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-   // }
+	Engine.run(engine);
+  
 }
 
 
-function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  
+  drawSprites();
+  
+  
+ 
 }
 
-function keyPressed(){
-    if(keyCode === 32 && bird.body.speed<1){
-        bird.trajectory =[]
-        Matter.Body.setPosition(bird.body,{x:200,y:50})
-        slingshot.attach(bird.body);
+function keyPressed()
+{
 
-    }
+    if(keyCode == LEFT_ARROW){
+		helicopterSprite.x=helicopterSprite.x-20
 
-}
+		Matter.Body.translate(packageBody, {x:-20,y:0})
+	}
 
-async function getBackgroundImg(){
-var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Tokyo")
-var json1response = await response.json()
-var dateTime = json1response.datetime
-var hour = dateTime.slice(11,13)
+	else if(keyCode == RIGHT_ARROW){
+		helicopterSprite.x=helicopterSprite.x+20
 
-if(hour >= 6 && hour <=19){
-    bg = "sprites/bg2.jpg"
-}
-else{
-    bg = "sprites/bg.png"
-}
+		Matter.Body.translate(packageBody, {x:+20,y:0})
 
-backgroundImg = loadImage(bg)
+	}
 
+	if(keyCode == DOWN_ARROW){
+		Matter.Body.setStatic(packageBody,false)
+	}
+	
 
 }
